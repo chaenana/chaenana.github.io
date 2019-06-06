84da009b08c05867ca5ecbbd1f6cc07816b0c77c
chaenana <48302992+chaenana@users.noreply.github.com>
2019-06-06 12:42:25 +0900

    슬라이드 jquery 추가

--- js/js.js
+++ js/js.js
@@ -1,1 +1,26 @@
 console.log("load");
+$(document).ready(function(){
+    // Add smooth scrolling to all links
+    $("a").on('click', function(event) {
+  
+      // Make sure this.hash has a value before overriding default behavior
+      if (this.hash !== "") {
+        // Prevent default anchor click behavior
+        event.preventDefault();
+  
+        // Store hash
+        var hash = this.hash;
+        //hash 라는건 알아서 다음꺼로 계산을 해주는? 그런거인듯함
+  
+        // Using jQuery's animate() method to add smooth page scroll
+        // The optional number (800) specifies the number of milliseconds it takes to scroll to the specified area
+        $('html, body').animate({
+          scrollTop: $(hash).offset().top
+        }, 700, function(){
+     
+          // Add hash (#) to URL when done scrolling (default click behavior)
+          window.location.hash = hash;
+        });
+      } // End if
+    });
+  });
