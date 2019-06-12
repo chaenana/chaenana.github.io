9b580660d56ee45401991e4c8fa3f0b46fcf30ab
chaenana <48302992+chaenana@users.noreply.github.com>
2019-06-13 00:03:22 +0900

    1차 레이아웃

--- 연습용파일/js/js.js
+++ 연습용파일/js/js.js
@@ -1,138 +1,49 @@
-
-var $gallery = document.querySelector('#gallery');
-
-var $view = $gallery.querySelector(".view");
-var $viewContainer = $view.querySelector(".view-container");
-var $viewItem = $viewContainer.querySelectorAll(".view-item");
-
-var $list = $gallery.querySelector(".list");
-var $listItem = $list.querySelectorAll("li");
-var _$a = [];
-
-
-
-var $paddlenav = $gallery.querySelector(".paddle-nav");
-var $btnPaddleEl = $paddlenav.querySelectorAll("button");
-var $btnPaddlePrevEl = $paddlenav.querySelector(".prev");
-var $btnPaddleNextEl = $paddlenav.querySelector(".next");
-
-
-
-
-
-
-var _vWidth = window.innerWidth;
-var _vHeight = window.innerHeight;
-
-var _max = $viewItem.length;
-var _cuId = 0;
-var _exId = _cuId;
-
-window.addEventListener('resize', onResize);
-function onResize() {
-    var _vWidth = window.innerWidth;
-    var _vHeight = window.innerHeight;
-    $viewContainer.style.width = (_vWidth * _max) + 'px';
-    for(var i =0; i < _max; i++){
-        $viewItem[i].style.width = _vWidth + 'px';
-    }
-    // 리사이즈 할 때, view-container 의 left 좌표 변경.
-    var left = _vWidth * _cuId * -1;
-    // $viewContainer.style.left = left + 'px';
-    TweenMax.killTweensOf($viewContainer); // 해당 요소의 TweenMax 애니메이션 정지.
-    TweenMax.set($viewContainer, {css: {left: left}});
-}
-onResize();
-
-for(var i=0; i< $btnPaddleEl.length; i++){
-    $btnPaddleEl[i].addEventListener("click", onClickPaddleEl)
-}
-
-function onClickPaddleEl(e){
-e.preventDefault();
-var $li = e.currentTarget.parentElement; //currenttarget 현재 자기 자신
-if($li.classList.contains('prev')){
-    
-    console.log("prev");
-    _cuId--;
-    if(_cuId < 0 ) _cuId = 0;
-}
-else if($li.classList.contains('next')){
-    console.log("next");
-    _cuId++;
-    if(_cuId >= _max ) _cuId = _max -1;
-}
-
-slide();
-}
-
-
-
-
-
-for(var i = 0; i < _max; i++){
-    var $a = $listItem[i].querySelector("a");
-    _$a.push($a);
-    $a.addEventListener("click", onClickListItemEl);
-}
-function onClickListItemEl(e) {
-    e.preventDefault();
-    var id = _$a.indexOf(e.currentTarget);
-    var $listItemParent = e.currentTarget.parentElement;
-    //현재 클릭된 요소(a) 의 부모 요소(li)를 찾는다.
-    if(!$listItemParent.classList.contains('selected')){
-        //부모 요소(li) 가 selected 라는 클래스를 포함하고 있는지 체크 - 조건에서는 포함하지 않을 때(false).
-        _cuId = id;
-        slide();
-      
-    }
-}
-
-$btnPaddlePrevEl.classList.add("disabled");
-function slide(){
-    if(_cuId === 0){
-        $btnPaddlePrevEl.classList.add("disabled");
-        $btnPaddleNextEl.classList.remove("disabled");
-        
-    }else if( _cuId === _max -1){
-        $btnPaddlePrevEl.classList.remove("disabled");
-        $btnPaddleNextEl.classList.add("disabled");
-
-    }else{
-        $btnPaddlePrevEl.classList.remove("disabled");
-        $btnPaddleNextEl.classList.remove("disabled");
-    }
-      //현재 클릭된 버튼의 번호를 변경.
-      $listItem[_exId].classList.remove("selected");
-      //이전에 활성화된 버튼의 클래스(selected)를 삭제
-      $listItem[_cuId].classList.add("selected");
-      //현재 버튼의 클래스(selected)가 활성화되도록 추가.
-      var left = _vWidth * _cuId * -1;
-
-      
-      //anime.
-      /*
-      anime({
-          targets : $viewContainer,
-          left : left,
-          duration : 1000,
-          easing : "easeInOutQuad"
-      });
-      */
-
-      //TweenMax.
-      //TweenMax.set();
-      //애니메이션 없이 스타일을 변경.
-      //TweenMax.set($viewContainer, { css: { left: left }});
-      //TweenMax.to();
-      //애니메이션을 설정.
-      TweenMax.to($viewContainer, 1.0, { css: { left: left }, ease: Quad.easeInOut });
-
-
-      //$viewContainer.style.left = left + 'px';
-
-
-
-      _exId = _cuId;
-      //다음 클릭시 눌렸던 번호를 유지하도록 값을 설정.
-}
+$(function(){
+   
+    // variable all elements
+  
+     var open  = $(".open"),
+         popup = $(".popup"),
+         main  = $(".main"),
+         close = $(".close"),
+         text  = $("h1"),
+         para  = $("p");
+  
+  // open window when click on open button
+  
+     open.on('click', function(){
+          main.fadeIn(1000);
+          popup.css('transform', 'scale(1, 1.4)');
+          popup.animate({width: '800px'}, 600, function(){
+                close.delay(600).fadeIn();
+                text.delay(700).fadeIn();
+                para.delay(700).fadeIn();
+             });
+  });
+  
+  // close window when click on close button
+  
+     close.on('click', function(){
+  
+          popup.css('transform', 'scale(1, 1)');
+          close.delay(100).fadeOut();
+          text.delay(100).fadeOut();
+          para.delay(100).fadeOut();
+          popup.animate({width: '100px'}, 600);
+          main.delay(1200).fadeOut();
+  });
+  
+  // close window on click anywhere
+   
+   main.on('click', function(){
+  
+          popup.css('transform', 'scale(1, 1)');
+          close.delay(100).fadeOut();
+          text.delay(100).fadeOut();
+          para.delay(100).fadeOut();
+          popup.animate({width: '100px'}, 600);
+          main.delay(1200).fadeOut();
+  
+  });
+  
+  });
