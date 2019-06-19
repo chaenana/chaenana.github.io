eac9efd657dd3ee74d47b1e85d9ce997edb72dee
chaenana <48302992+chaenana@users.noreply.github.com>
2019-06-20 02:15:26 +0900

    최종

--- js/js.js
+++ js/js.js
@@ -122,6 +122,24 @@ $(document).ready(function() {
 
 
 
+
+$(document).ready(function() {
+  $(window).scroll( function(){
+      $('.ph').each( function(i){
+          
+          var bottom_of_element = $(this).offset().top + $(this).outerHeight();
+          var bottom_of_window = $(window).scrollTop() + $(window).height();
+          
+          if( bottom_of_window > bottom_of_element ){
+              $(this).animate({'opacity':'1','margin-top':'0px'}, 1500);
+          }
+          
+      }); 
+  });
+});
+
+
+
 //마우스 커서
 var $cursorDot = document.querySelector("#cursor-dot");
 var $cursorBG = document.querySelector("#cursor-bg");
